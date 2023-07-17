32c59f67db1e47a7ebe5583719d862691b32a785
Rumon <mdsrrumon1234@gmail.comm>
2023-07-17 23:50:59 +0600

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,40 @@
-import Content from "./Component/Content";
-import Footer from "./Component/Footer";
-import Header from "./Component/Header";
-
+import { useState } from "react";
 const App = () => {
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState('');
+  const handleInputChange = (event) => {
+    setNewTask(event.target.value);
+  };
+  const addTask = (event) => {
+    event.preventDefault();
+    if (newTask.trim() !== '') {
+      setTasks([...tasks, newTask]);
+      setNewTask('');
+    }
+  };
+  const removeTask = (index) => {
+    setTasks(tasks.filter((_, i) => i !== index));
+  };
 
   return (
     <div>
-      <Header title="This is My First React App" />
-      <Content />
-      <Footer />
+      <form onSubmit={addTask}>
+        <input
+          type="text"
+          value={newTask}
+          onChange={handleInputChange}
+          placeholder="Enter a new task"
+        />
+        <button type="submit">Add Task</button>
+      </form>
+      <ul>
+        {tasks.map((task, index) => (
+          <li key={index}>
+            {task}
+            <button onClick={() => removeTask(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
--- src/Component/Footer.jsx
+++
@@ -1,4 +0,0 @@
-const Footer = () => {
-    return <p>This is footer Section ...........</p>;
-};
-export default Footer;
